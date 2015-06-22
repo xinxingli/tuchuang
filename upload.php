eda2e8539b4575fd1181978729ef4d7df98f4bfc
li xinxing <lxx19950227@163.com>
2015-06-23 00:55:33 +0800

    Create upload.php
    文件上传主界面

---
+++ upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>上传文件</title>
+</head>
+<form action="doaction1.php" method="post" enctype="multipart/form-data">
+请选择您要上传的文件：
+<input type="file" name="myFile" /><br />
+<input type="submit" value="上传文件" />
+</form>
+<body>
+</body>
+</html>
